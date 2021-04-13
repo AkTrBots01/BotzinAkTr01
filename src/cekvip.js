aaf4a86dfb0c6a85e54a52f78bd8b78fc8e37714
AkTrBots01 <viniciuswandson05@gmail.com>
2021-04-13 01:38:10 -0300

    first commit

---
+++ src/cekvip.js
@@ -0,0 +1,15 @@
+const cekvip = () => { 
+	return `           
+──────────────────
+*Nome do bot* :  ANA BOT 🌝
+──────────────────
+        『 *𝐕𝐈𝐏 𝐔𝐒𝐄𝐑* 』
+──────────────────
+• *Status*    : *ATIVO*
+────────────────── 
+*Status Bot:* *Online*
+──────────────────
+
+*VOCE E UM MEMBRO PREMIUM* POHA `
+}
+exports.cekvip = cekvip
